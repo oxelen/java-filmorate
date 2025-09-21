9822d9cb614a4912c13fadc93a7b43edd5feb68c
oxelen <boyar.f@yandex.ru>
2025-09-21 12:44:47 +0300

    init ТЗ 11

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
 import java.time.Period;
