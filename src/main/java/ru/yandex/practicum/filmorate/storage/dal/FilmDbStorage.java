05ed96e01aec3b863099b453f72e2c883253ea01
oxelen <boyar.f@ya.ru>
2025-11-17 19:58:29 +0300

    upd add-reviews

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmDbStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.dal;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
