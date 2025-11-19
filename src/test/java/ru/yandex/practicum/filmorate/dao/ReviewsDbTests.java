529495491e8cfd1b92b2833fd6c8183f4d15dd03
oxelen <boyar.f@ya.ru>
2025-11-19 21:58:28 +0300

    upd add-reviews

--- src/test/java/ru/yandex/practicum/filmorate/dao/ReviewsDbTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/ReviewsDbTests.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.filmorate.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.model.Review;
